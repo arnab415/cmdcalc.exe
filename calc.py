fc77b7d40ba57bd053483ac0d75868b0af5ce954
arnab415 <75836387+arnab415@users.noreply.github.com>
2021-01-21 13:30:31 +0530

    Add files via upload

---
+++ calc.py
@@ -0,0 +1,28 @@
+import sys
+import argparse as ap
+
+def calc(args):
+    if args.o == "add":
+        return args.f + args.s
+    elif args.o == "sub":
+        return args.f - args.s
+    elif args.o == "mul":
+        return args.f * args.s
+    elif args.o == "div":
+        return args.f / args.s
+    elif args.o == "div":
+        return args.f / args.s
+    else:
+        return "Something went wrong"
+if __name__ == '__main__':
+        parser = ap.ArgumentParser()
+        parser.add_argument("--f", type=float, default=1.0,
+                            help=("Please enter first number."))
+        parser.add_argument("--s", type=float, default=3.0,
+                            help=("Please enter second number."))
+        parser.add_argument("--o", type=str, default="",
+                            help=("Please enter the operation."))
+
+
+        args = parser.parse_args()
+        sys.stdout.write(str(calc(args)))
